7fa063b56e4317ed9aa4ba4bc0a5c05a86e259b6
konata9 <maochichen@163.com>
2019-01-12 14:35:46 +0800

    complete

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'typecheck.js',
+    libraryTarget: 'commonjs2',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        exclude: [
+          path.resolve(__dirname, './node_modules')
+        ],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
