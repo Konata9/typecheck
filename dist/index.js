a3a7161823244f266a230b0f586860ba476bcf77
konata9 <maochichen@163.com>
2019-04-29 23:31:07 +0800

    change config

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function typeCheck(params) {
-    const typeStr = Object.prototype.toString.call(params);
+    var typeStr = Object.prototype.toString.call(params);
     return typeStr.toLowerCase().slice(8, typeStr.length - 1);
 }
 exports.typeCheck = typeCheck;
